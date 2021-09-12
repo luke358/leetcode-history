13b5dcbf7ba07d8354db775020a6dacee8b58764
zxc <1494135711@qq.com>
2021-09-12 21:57:31 +0800

    first commit1

--- 222. 完全二叉树的节点个数.js
+++ 222. 完全二叉树的节点个数.js
@@ -25,4 +25,23 @@ function getNodeSum(root) {
 	return leftNodeSum + rightNodeSUm + 1;
 }
 
-// 层序遍历
+// 层序遍历   注意 这个时候不需要分左右， 所以直接用 栈 就可以了
+var countNodes = function (root) {
+	if (!root) return 0;
+
+	let stack = [root];
+
+	let nodeSum = 0
+	while (stack.length) {
+		let len = stack.length;
+		while (len--) {
+			let cur = stack.pop();
+			nodeSum++;
+			cur.left && stack.push(cur.left);
+			cur.right && stack.push(cur.right);
+		}
+	}
+	return nodeSum;
+};
+
+// 利用完全二叉树的性质
