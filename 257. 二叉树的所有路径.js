a329b0e7846ea785adcd973bcd3bc3f859d1908b
zhuxuechang <zhuxuechang@kedacom.com>
2021-09-13 12:01:36 +0800

    257. 二叉树的所有路径 迭代版

--- 257. 二叉树的所有路径.js
+++ 257. 二叉树的所有路径.js
@@ -45,3 +45,45 @@ function traverse(root, res, path) {
 		path.pop()
 	}
 }
+
+
+// 迭代法
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {string[]}
+ */
+var binaryTreePaths = function (root) {
+	let res = [];
+	let path = [root.val.toString()];
+	let stack = [root]
+	if (!root) return res;
+
+	while (stack.length) {
+		let cur = stack.pop();
+		// 当前节点所拼接的路径
+		let p = path.pop();
+
+		// 走到叶子节点了
+		if (!cur.left && !cur.right) {
+			res.push(p);
+		}
+		// 要记住 p 是父级拼接的路径，  子集只需要再他们的基础上加上就ok了
+		if (cur.right) {
+			stack.push(cur.right);
+			path.push(p + '->' + cur.right.val);
+		}
+		if (cur.left) {
+			stack.push(cur.left);
+			path.push(p + '->' + cur.left.val);
+		}
+	}
+	return res;
+};
