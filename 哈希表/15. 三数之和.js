babec708493baaa3f531aa1ddb952ef80bcab586
zhuxuechang <1494135711@qq.com>
2022-06-30 16:44:29 +0800

    feat: 15. 三数之和

---
+++ 哈希表/15. 三数之和.js
@@ -0,0 +1,60 @@
+/*
+给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？请你找出所有和为 0 且不重复的三元组。
+
+注意：答案中不可以包含重复的三元组。
+
+输入：nums = [-1,0,1,2,-1,-4]
+输出：[[-1,-1,2],[-1,0,1]]
+
+输入：nums = []
+输出：[]
+
+输入：nums = [0]
+输出：[]
+
+拿这个nums数组来举例，首先将数组排序，然后有一层for循环，i从下标0的地方开始，同时定一个下标left 定义在i+1的位置上，定义下标right 在数组结尾的位置上。
+
+依然还是在数组中找到 abc 使得a + b + c =0，我们这里相当于 a = nums[i] b = nums[left] c = nums[right]。
+
+接下来如何移动left 和right呢， 如果nums[i] + nums[left] + nums[right] > 0 就说明 此时三数之和大了，因为数组是排序后了，所以right下标就应该向左移动，这样才能让三数之和小一些。
+
+如果 nums[i] + nums[left] + nums[right] < 0 说明 此时 三数之和小了，left 就向右移动，才能让三数之和大一些，直到left与right相遇为止。
+
+时间复杂度：$O(n^2)$。
+*/
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function (nums) {
+  nums.sort((a, b) => a - b)
+  let result = [];
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] > 0) return result;
+
+    if (i > 0 && nums[i] === nums[i - 1]) continue;
+
+    let left = i + 1, right = nums.length - 1;
+    while (left < right) {
+      if (nums[i] + nums[right] + nums[left] > 0) {
+        right--;
+      } else if (nums[i] + nums[right] + nums[left] < 0) {
+        left++;
+      } else {
+        result.push([nums[i], nums[left], nums[right]]);
+        // 去重
+        while (left < right && nums[left] === nums[left + 1]) {
+          left++;
+        }
+        while (left < right && nums[right] === nums[right - 1]) {
+          right--;
+        }
+        left++;
+        right++;
+      }
+    }
+  }
+  return result;
+};
+
+threeSum([-1,0,1,2,-1,-4])
